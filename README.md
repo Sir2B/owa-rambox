5128ff10d0af94fffee4c62fefdbdcc2a3370d54
Tobias Obermayer <tobias.obermayer@gmail.com>
2021-03-18 07:43:01 +0100

    support for OWA 2011
    supports unread count and number of calendar notifications

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Exchange OWA support in Rambox
 
+- fork for supporting OWA version of 2011 with had no automatic login
+
 How to use:
 
-1. Choose _Custom Service
+- 1. Choose _Custom Service
 
-* _Name:_ Whatever you like
-* _URL:_ [https://[your.domain.com]/owa/](https://%5Byour.domain.com%5D/owa/)
-* _Logo:_ [https://www.jwcc.edu/files/Information_Technology/images/outlook2k13logo.png](https://www.jwcc.edu/files/Information_Technology/images/outlook2k13logo.png), though you can use any you like.
+  - _Name:_ Whatever you like
+  - _URL:_ [https://[your.domain.com]/owa/](https://%5Byour.domain.com%5D/owa/)
+  - _Logo:_ [https://www.jwcc.edu/files/Information_Technology/images/outlook2k13logo.png](https://www.jwcc.edu/files/Information_Technology/images/outlook2k13logo.png), though you can use any you like.
 
-2. Open _Advanced_ and use these Custom Codes (depending on your feature preferences):
+- 2. Open _Advanced_ and use this Custom Codes (depending on your feature preferences):
 
-* Unread count only, immediately updated - paste the contents of **unread-notification.js**
-* Unread count, incoming mail and calendar reminder notifications - paste the contents of **new-mail-notification.js**
+- Unread count, and calendar reminder notifications - adjust username, password in the contents of **unread-notification.js**, then paste the result in Rambox
 
 Ideas and pull requests are welcome.
--- new-mail-notification.js
+++ new-mail-notification.js
@@ -1,101 +1,62 @@
 setTimeout(() => {
-    const target = $("[title=Inbox]").first().next().next()[0];
-
-    const unreadObserver = new MutationObserver(() => {
-        updateBadge(target.textContent);
-    });
-
-    const newNotificationObserver = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            if (mutation.addedNodes.length) {
-                Array.prototype.forEach.call(mutation.addedNodes, (node) => {
-                    if (node.getAttribute("ispopup") === "1") {
-                        const element = $(node);
-
-                        if (harvestNotifications(element)) {
-                            attachPopupObserver(element.children().children()[0]);
-                        }
-                    }
-                });
-            }
-        });
-    });
-
-    function harvestNotifications(node) {
-        const root = $(node).find(".headerMenuDropShadow");
-
-        let info = [];
-
-        info = root.find("> button > span")
-            .map((index, element) => {
-                return element.textContent;
-            }).toArray();
-
-        if (info.length) {
-            info.shift();
-
-            showNotification(info);
-        }
-
-        root.find("[aria-label=Reminders] > div > div > div").each((index, element) => {
-            info = $(element).find("div > span > span")
-                .map((index, element) => {
-                    return element.textContent;
-                }).toArray();
-
-            if (info.length) {
-                info.push($(element).find("div > span > div > div:not([style*='display: none']) > span")
-                    .map((index, element) => {
-                        return element.textContent;
-                    }).toArray().join(" "));
-            }
-
-            showNotification(info);
-        });
-
-        return !!root.length;
-    }
-
-    function attachPopupObserver(node) {
-        const popupMutationObserver = new MutationObserver((mutations) => {
-            mutations.forEach((mutation) => {
-                Array.prototype.forEach.call(mutation.addedNodes, (node) => {
-                    harvestNotifications(node);
-                });
-            });
-        });
-
-        popupMutationObserver.observe(node, { childList: true });
-    }
-
+    const username = "";
+    const password = "";
+    
+    var originalTitle = document.title;
     function showNotification(info) {
         new Notification(info[0], {
             icon: "http://i.imgur.com/lJ4LbVp.png",
             body: `Subject: ${info[1]}\n${info[2]}`
         });
     }
-
-    function updateBadge(count) {
-        document.title = document.title.replace(/^\(\d+\)/g, "");
-
-        if (count) {
-            document.title = `(${count}) ${document.title}`;
+    
+    function LoginIfNecessary() {
+        if (document.getElementById('mainLogonDiv')) {
+            document.getElementById('username').value = username;
+            document.getElementById('password').value = password;
+            
+            document.getElementsByClassName('signinbutton')[0].click();
         }
     }
-
-    const popups = $("body > [ispopup=1]");
-
-    popups.each((index, node) => {
-        const element = $(node);
-
-        if (harvestNotifications(element)) {
-            attachPopupObserver(element.children().children()[0]);
+    
+    function getMailCount() {
+        const mailSelector = "[title=Posteingang]";
+        let span = document.querySelector(mailSelector);
+        while (span) {
+            const value = span.innerText;
+            if (isNumeric(value)) {
+                return parseInt(value);
+            }
+            span = span.nextElementSibling;
         }
-    });
-
-    updateBadge(target.textContent);
-
-    unreadObserver.observe(target, { subtree: true, characterData: true });
-
-    newNotificationObserver.observe(document.body, { childList: true });
-}, 3000);
+        return 0;
+    }
+    
+    function getNotificationCount() {
+        const span = document.querySelector('span._f_k4');
+        if (span) {
+            return parseInt(span.innerText) || 0;
+        }
+        return 0;
+    }
+    
+    function updateBadge(e) {
+        e >= 1 ? document.title = "(" + e + ") " + originalTitle : document.title = originalTitle
+    }
+    
+    function isNumeric(value) {
+        return !isNaN(value) && !isNaN(parseFloat(value));
+    }
+    
+    LoginIfNecessary();
+    
+    setInterval(() => {
+        LoginIfNecessary();
+    
+        const notifications = getNotificationCount();
+        const mails = getMailCount();
+        const badgeCnt = notifications + mails;
+        updateBadge(badgeCnt);
+    }, 3000);
+    
+}, 100);
